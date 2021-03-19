aa7d1861df48287a23eab0eb963775798d4e0e77
TanvirAhmed177 <ahmed.tanvir.siddik@gmail.com>
2021-03-20 03:53:18 +0600

    Update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../../App";
 
 const Header = () => {
-  const [user, setUser] = useContext(UserContext);
+  const [user] = useContext(UserContext);
 
   return (
     <Container fluid>
